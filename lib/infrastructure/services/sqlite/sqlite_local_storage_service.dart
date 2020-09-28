1974ad157a6422cf8174f26f331f191f1093e1ef
Claudney Sarti Sesa <claudneysartisessa@gmail.com>
2020-09-27 21:15:37 -0300

    Criando os Services

---
+++ lib/infrastructure/services/sqlite/sqlite_local_storage_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutterando_arquitetura/infrastructure/interfaces/local_storage_interface.dart';
+
+class SqliteLocalStorageService implements ILocalStorage {
+  @override
+  Future delete(String key) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future get(String key) {
+    // TODO: implement get
+    throw UnimplementedError();
+  }
+
+  @override
+  Future put(String key, value) {
+    // TODO: implement put
+    throw UnimplementedError();
+  }
+}
--- lib/presentation/application/controllers/application_controller.dart
+++ lib/presentation/application/controllers/application_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutterando_arquitetura/infrastructure/interfaces/local_storage_interface.dart';
 import 'package:flutterando_arquitetura/infrastructure/models/application_config_model.dart';
+import 'package:flutterando_arquitetura/infrastructure/services/shared_preferences/shared_local_storage_Service.dart';
 
 //Singleton
 
@@ -12,13 +14,24 @@ class ApplicationController {
   //Construtor privado
   //Acesso apenas dentro da classe
   //Evita que se possa criar uma instância do singleton
-  ApplicationController._();
+  ApplicationController._() {
+    configStorage.get('isDark').then(
+      (value) {
+        if (value != null) {
+          config.themeSwitch.value = value;
+        }
+      },
+    );
+  }
 
   final ApplicationConfigModel config = ApplicationConfigModel();
   bool get isDark => config.themeSwitch.value;
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
+  final ILocalStorage configStorage = SharedLocalStorageService();
+
   changeTheme(bool value) {
     config.themeSwitch.value = value;
+    configStorage.put('isDark', value);
   }
 }
